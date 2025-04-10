2eb7920d5ac4c4b085c13c1f6c5b12c439a9a08f
Zalman Ul Fariz P A <zalman15320011351@gmail.com>
2025-04-10 15:52:24 +0530

    Defined Vulnerable & Target functions

--- Src/main.c
+++ Src/main.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "UART.h"
+#include "LED.h"
 #include "SYSTICK.h"
 
-
-int main()
+void VulnerableFunction(void)
 {
-	UART2_Init();
+    char buffer[32];  // Small stack buffer
 
-	printf("\n\n\n EXECUTING APPLICATION CODE\n\r");
-	delay_ms(5000);
+    UART2_TxString("Enter data: ");
 
-	while(1)
-	{
-		printf("Hello World\n\r");
-		delay_ms(1000);
-	}
+    // 👇 Using the safe driver unsafely: overflow incoming!
+    UART2_RxString((uint8_t*)buffer, 100);  // Overwrites past 32 bytes
+
+    UART2_TxString("Done.\n");
+}
+
+void TargetFunction(void)
+{
+    while (1)
+    {
+    	LED_Toggle();
+    	delay_ms(250);
+    }
+}
+
+int main(void)
+{
+    UART2_Init();
+    LED_Init();
+
+    UART2_TxString("System Ready.\n");
+
+    VulnerableFunction();
+
+    while (1)
+    {
+
+    }
 }
